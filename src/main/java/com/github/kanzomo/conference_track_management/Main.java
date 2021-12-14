930ff90450bf11d5d8ac56f1831d5ba9e69bf9e0
kanzomo <94554333+kanzomo@users.noreply.github.com>
2021-12-14 15:46:53 +0100

    Implement Main, filepath as CLI argument

--- src/main/java/com/github/kanzomo/conference_track_management/Main.java
+++ src/main/java/com/github/kanzomo/conference_track_management/Main.java
@@ -1,9 +1,28 @@
 package com.github.kanzomo.conference_track_management;
 
 import java.io.FileNotFoundException;
+import java.util.List;
+
+import com.github.kanzomo.conference_track_management.io.FileReader;
+import com.github.kanzomo.conference_track_management.io.Writer;
 
 public class Main {
-	public static void main(String[] args) throws FileNotFoundException{
+	public static void main(String[] args) throws FileNotFoundException {
+		String filePath = null;
+		if (args.length == 0) {
+			System.out.println("Try again with file path as argument.");
+			return;
+		} else {
+			filePath = args[0];
+		}
 		
+		List<Talk> talks = FileReader.getInstance().read(filePath);
+		Conference conference = new Conference();
+		conference.buildSchedule(talks);
+		Writer writer = new Writer(conference);
+
+		writer.toConsole();
 	}
+	
+
 }
